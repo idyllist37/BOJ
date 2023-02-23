dded542c556133128547cd458cf721429fb4ae3b
FuyuCat <100647714+idyllist37@users.noreply.github.com>
2023-02-23 12:58:10 +0900

    Create 10871.cpp

---
+++ 10871.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+ 
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+   int n, x, a;
+   cin >> n >> x;
+   for(int i = 0; i < n ; i++) { 
+      cin >> a;
+      if(a < x) {
+         cout << a << " ";
+      }
+   }
+   
+}
