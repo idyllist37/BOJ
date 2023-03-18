62d323e7bcb2a03818d27a9d1fa34dfad3e811c0
FuyuCat <100647714+idyllist37@users.noreply.github.com>
2023-03-18 23:58:28 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 72 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define X first
+#define Y second
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(nullptr);
+    
+  int n;
+  cin >> n;
+  stack<pair<int, int>> S;
+  long long ans = 0;
+    
+  while (n--) {
+    int h;
+    cin >> h;
+    int cnt = 1;
+      
+    while (!S.empty() && S.top().X <= h) {
+      ans += S.top().Y;
+        
+      if (S.top().X == h) cnt += S.top().Y;
+      S.pop();
+    }
+      
+    if (!S.empty()) ans++;
+    S.push({h, cnt});
+  }
+  cout << ans;
+}
