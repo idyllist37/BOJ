7da7f85a13b1ff7c08615c1147bbffcd5ecbba13
FuyuCat <100647714+idyllist37@users.noreply.github.com>
2023-03-18 23:54:17 +0900

    [Silver IV] Title: 제로, Time: 8 ms, Memory: 2548 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int K, ans = 0;
+stack<int> s;
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(nullptr);
+
+  cin >> K;
+  while(K--){
+    int n;
+    cin >> n;
+    if(n == 0) s.pop();
+    else s.push(n);
+  }
+
+
+    while(!s.empty()){
+    ans += s.top();
+    s.pop();
+  }
+  cout << ans;
+}
